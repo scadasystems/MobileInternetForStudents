bc5b519426610e5f11042627316b68e55161d0bd
LulzM <yunmo159789@gmail.com>
2019-09-19 20:36:56 +0900

    .
    .

---
+++ app/src/main/java/com/example/mobileinternetforstudent/exampleFirebase/Post.kt
@@ -0,0 +1,23 @@
+package com.example.mobileinternetforstudent.exampleFirebase
+
+/*********************************************************
+ * $$ \                               $$ \                      $$\            $$\
+ * $$ |                               $$ |                      $$$\        $$$ |
+ * $$ |          $$\       $$ \  $$ |  $$$$$$$$\   $$$$\    $$$$ |
+ * $$ |          $$ |      $$ |  $$ |  \_____ $$    |   $$ \$$\$$ $$ |
+ * $$ |          $$ |      $$ |  $$ |     $$$$ __/     $$   \$$$  $$ |
+ * $$ |          $$ |      $$ |  $$ |   $$  ___/        $$ |  \$    /$$ |
+ * $$$$$$$$\\$$$$$$   |  $$ | $$$$$$$$\    $$ |   \__/  $$ |
+ * \_____________|\_________/    \___|  \____________|    \__|           \__|
+ *
+ * Project : MobileInternetForStudent
+ * Created by Android Studio
+ * Developer : Lulz_M
+ * Date : 2019-09-19
+ * Time : 19:46
+ * GitHub : https://github.com/scadasystems
+ * E-mail : redsmurf@lulzm.org
+ *********************************************************/
+data class Post(
+        var imageUrl:String = ""
+)
--- app/src/main/java/com/example/mobileinternetforstudent/fragments/FirebaseFragment.kt
+++ app/src/main/java/com/example/mobileinternetforstudent/fragments/FirebaseFragment.kt
@@ -58,4 +58,6 @@ class FirebaseFragment : Fragment() {
             transaction.commit()
         }
     }
+
+
 }
